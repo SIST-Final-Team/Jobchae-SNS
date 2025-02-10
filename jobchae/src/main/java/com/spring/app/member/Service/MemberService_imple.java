9a65994f27b14fe452741f02e2e68bf6b2bc04ac
dlwnsdud1853 <110625903+minjun9492@users.noreply.github.com>
2025-02-10 18:21:32 +0900

    feat:회원 컨트롤러, 도메인, 모델, 서비스 생성 완료

---
+++ jobchae/src/main/java/com/spring/app/member/Service/MemberService_imple.java
@@ -0,0 +1,17 @@
+package com.spring.app.member.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.spring.app.member.model.MemberDAO;
+
+public class MemberService_imple implements MemberService {
+
+	@Autowired
+	MemberDAO dao;
+	
+	
+	
+	
+	
+	
+}//end of class..
---
+++ jobchae/src/main/java/com/spring/app/member/model/MemberDAO.java
@@ -0,0 +1,10 @@
+package com.spring.app.member.model;
+
+public interface MemberDAO {
+
+	
+	
+	
+	
+	
+}//end of interface...
