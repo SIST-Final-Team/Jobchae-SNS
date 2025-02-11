8c5a10a3f626db57ea8ca4522e752bffc5184e09
gyu9907 <gyu9907@gmail.com>
2025-02-11 09:43:43 +0900

    chore: 포트 80으로 변경 및 버그 수정

--- jobchae/src/main/java/com/spring/app/member/model/MemberDAO_imple.java
+++ jobchae/src/main/java/com/spring/app/member/model/MemberDAO_imple.java
@@ -3,7 +3,9 @@ package com.spring.app.member.model;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class MemberDAO_imple implements MemberDAO {
 
 	
