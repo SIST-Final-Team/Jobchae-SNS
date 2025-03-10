db7317de7a7ba867e954f51fb7b5886ffa437cf9
gyu9907 <gyu9907@gmail.com>
2025-03-10 16:50:20 +0900

    fix: SearchDAO: 팔로우 join 논리적 문제 해결

--- jobchae/src/main/java/com/spring/app/search/model/SearchDAO.java
+++ jobchae/src/main/java/com/spring/app/search/model/SearchDAO.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.spring.app.file.domain.FileVO;
 import com.spring.app.search.domain.SearchBoardVO;
 
 @Mapper
@@ -15,13 +14,12 @@ public interface SearchDAO {
 	 * 게시물 검색 결과 가져오기
 	 * @param paraMap
 	 * searchWord: 검색어,
-	 * login_member_id: 로그인 한 유저,
-	 * board_register_date: 작성일 범위,
-	 * searchFileType: 첨부 파일 확장자,
+	 * login_member_id: 로그인 한 유저 id,
+	 * board_register_date: 작성일 범위(현재로부터 n일 전),
+	 * searchFileType: 첨부 파일 타입(image, video),
 	 * start: 시작 번호, end: 끝 번호
 	 * @return
 	 */
 	List<SearchBoardVO> searchBoardByContent(Map<String, String> paraMap);
-	
 	
 }
